bbb3653359733801c1e94b4f380bd29314d24a54
navleen <136760730+navleenkaurr@users.noreply.github.com>
2024-05-10 21:02:02 -0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// stdio.h is a file which contains input-output header files
+// which contains functions like printf(), scanf()... that are used to perform inputs and output operations.
+
+void main(){
+    printf("Welcome to coding world.\n");
+    printf("We are going to learn the basis of C language.");
+}
